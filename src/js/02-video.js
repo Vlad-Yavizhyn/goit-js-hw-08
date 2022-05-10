314cc4884d6b436dcc8805e41d537dbcc087ea7d
Vladlen <91382235+Vlad-Yavizhyn@users.noreply.github.com>
2022-05-10 20:05:55 +0300

    video done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+    const CURRENT_TIME = 'videoplayer-current-time';
+const onPlay = function (data) {
+  localStorage.setItem(CURRENT_TIME, data.seconds);
+};
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const playedTime = localStorage.getItem(CURRENT_TIME);
+
+player.setCurrentTime(Number(playedTime)).then(function (seconds) {
+  seconds = Number(playedTime);
+});
